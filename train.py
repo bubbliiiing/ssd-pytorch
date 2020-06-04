24def704980cfae70e690f5dd8e6b6a530512548
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-06-04 17:31:27 +0800

    Update train.py

--- train.py
+++ train.py
@@ -34,8 +34,9 @@ if __name__ == "__main__":
     model = get_ssd("train",Config["num_classes"])
 
     print('Loading weights into state dict...')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model_dict = model.state_dict()
-    pretrained_dict = torch.load("model_data/ssd_weights.pth")
+    pretrained_dict = torch.load("model_data/ssd_weights.pth", map_location=device)
     pretrained_dict = {k: v for k, v in pretrained_dict.items() if np.shape(model_dict[k]) ==  np.shape(v)}
     model_dict.update(pretrained_dict)
     model.load_state_dict(model_dict)
