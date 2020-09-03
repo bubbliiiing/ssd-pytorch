f8512c37038c7fc0fc491d1d17d381e3c03b7bc2
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-09-03 17:31:34 +0800

    Update train.py

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ from utils.dataloader import ssd_dataset_collate, SSDDataset
 from utils.config import Config
 from torchsummary import summary
 from torch.autograd import Variable
+from tqdm import tqdm
 import torch.backends.cudnn as cudnn
 import time
 import torch
@@ -12,16 +13,11 @@ import numpy as np
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.init as init
-def adjust_learning_rate(optimizer, lr, gamma, step):
-    lr = lr * (gamma ** (step))
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
-    return lr
 
-#----------------------------------------------------#
-#   检测精度mAP和pr曲线计算参考视频
-#   https://www.bilibili.com/video/BV1zE411u7Vw
-#----------------------------------------------------#
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+        
 if __name__ == "__main__":
     # ------------------------------------#
     #   先冻结一部分权重训练
@@ -44,10 +40,7 @@ if __name__ == "__main__":
     Use_Data_Loader = True
 
     model = get_ssd("train",Config["num_classes"])
-    
-    #-------------------------------------------#
-    #   权值文件的下载请看README
-    #-------------------------------------------#
+
     print('Loading weights into state dict...')
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model_dict = model.state_dict()
@@ -83,15 +76,6 @@ if __name__ == "__main__":
                              False, Cuda)
     epoch_size = num_train // Batch_size
 
-
-    #------------------------------------------------------#
-    #   主干特征提取网络特征通用，冻结训练可以加快训练速度
-    #   也可以在训练初期防止权值被破坏。
-    #   Init_Epoch为起始世代
-    #   Freeze_Epoch为冻结训练的世代
-    #   Epoch总训练世代
-    #   提示OOM或者显存不足请调小Batch_size
-    #------------------------------------------------------#
     if True:
         # ------------------------------------#
         #   冻结一定部分训练
@@ -100,40 +84,42 @@ if __name__ == "__main__":
             param.requires_grad = False
 
         optimizer = optim.Adam(net.parameters(), lr=lr)
+        lr_scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=1,gamma=0.9)
         for epoch in range(Start_iter,Freeze_epoch):
-            if epoch%2==0:
-                adjust_learning_rate(optimizer,lr,0.9,epoch)
-            loc_loss = 0
-            conf_loss = 0
-            for iteration, batch in enumerate(gen):
-                if iteration >= epoch_size:
-                    break
-                images, targets = batch[0], batch[1]
-                with torch.no_grad():
-                    if Cuda:
-                        images = Variable(torch.from_numpy(images).type(torch.FloatTensor)).cuda()
-                        targets = [Variable(torch.from_numpy(ann).type(torch.FloatTensor)).cuda() for ann in targets]
-                    else:
-                        images = Variable(torch.from_numpy(images).type(torch.FloatTensor))
-                        targets = [Variable(torch.from_numpy(ann).type(torch.FloatTensor)) for ann in targets]
-                # 前向传播
-                out = net(images)
-                # 清零梯度
-                optimizer.zero_grad()
-                # 计算loss
-                loss_l, loss_c = criterion(out, targets)
-                loss = loss_l + loss_c
-                # 反向传播
-                loss.backward()
-                optimizer.step()
-                # 加上
-                loc_loss += loss_l.item()
-                conf_loss += loss_c.item()
+            with tqdm(total=epoch_size,desc=f'Epoch {epoch + 1}/{Freeze_epoch}',postfix=dict,mininterval=0.3) as pbar:
+                loc_loss = 0
+                conf_loss = 0
+                for iteration, batch in enumerate(gen):
+                    if iteration >= epoch_size:
+                        break
+                    images, targets = batch[0], batch[1]
+                    with torch.no_grad():
+                        if Cuda:
+                            images = Variable(torch.from_numpy(images).type(torch.FloatTensor)).cuda()
+                            targets = [Variable(torch.from_numpy(ann).type(torch.FloatTensor)).cuda() for ann in targets]
+                        else:
+                            images = Variable(torch.from_numpy(images).type(torch.FloatTensor))
+                            targets = [Variable(torch.from_numpy(ann).type(torch.FloatTensor)) for ann in targets]
+                    # 前向传播
+                    out = net(images)
+                    # 清零梯度
+                    optimizer.zero_grad()
+                    # 计算loss
+                    loss_l, loss_c = criterion(out, targets)
+                    loss = loss_l + loss_c
+                    # 反向传播
+                    loss.backward()
+                    optimizer.step()
+                    # 加上
+                    loc_loss += loss_l.item()
+                    conf_loss += loss_c.item()
 
-                print('\nEpoch:'+ str(epoch+1) + '/' + str(Freeze_epoch))
-                print('iter:' + str(iteration) + '/' + str(epoch_size) + ' || Loc_Loss: %.4f || Conf_Loss: %.4f ||' % (loc_loss/(iteration+1),conf_loss/(iteration+1)), end=' ')
-                
-                
+                    pbar.set_postfix(**{'loc_loss'  : loc_loss / (iteration + 1), 
+                                        'conf_loss' : conf_loss / (iteration + 1),
+                                        'lr'        : get_lr(optimizer)})
+                    pbar.update(1)
+                        
+            lr_scheduler.step()
             print('Saving state, iter:', str(epoch+1))
             torch.save(model.state_dict(), 'logs/Epoch%d-Loc%.4f-Conf%.4f.pth'%((epoch+1),loc_loss/(iteration+1),conf_loss/(iteration+1)))
 
@@ -145,38 +131,41 @@ if __name__ == "__main__":
             param.requires_grad = True
 
         optimizer = optim.Adam(net.parameters(), lr=freeze_lr)
+        lr_scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=1,gamma=0.9)
         for epoch in range(Freeze_epoch,Epoch):
-            if epoch%2==0:
-                adjust_learning_rate(optimizer,freeze_lr,0.9,epoch)
-            loc_loss = 0
-            conf_loss = 0
-            for iteration, batch in enumerate(gen):
-                if iteration >= epoch_size:
-                    break
-                images, targets = batch[0], batch[1]
-                with torch.no_grad():
-                    if Cuda:
-                        images = Variable(torch.from_numpy(images).type(torch.FloatTensor)).cuda()
-                        targets = [Variable(torch.from_numpy(ann).type(torch.FloatTensor)).cuda() for ann in targets]
-                    else:
-                        images = Variable(torch.from_numpy(images).type(torch.FloatTensor))
-                        targets = [Variable(torch.from_numpy(ann).type(torch.FloatTensor)) for ann in targets]
-                # 前向传播
-                out = net(images)
-                # 清零梯度
-                optimizer.zero_grad()
-                # 计算loss
-                loss_l, loss_c = criterion(out, targets)
-                loss = loss_l + loss_c
-                # 反向传播
-                loss.backward()
-                optimizer.step()
-                # 加上
-                loc_loss += loss_l.item()
-                conf_loss += loss_c.item()
+            with tqdm(total=epoch_size,desc=f'Epoch {epoch + 1}/{Freeze_epoch}',postfix=dict,mininterval=0.3) as pbar:
+                loc_loss = 0
+                conf_loss = 0
+                for iteration, batch in enumerate(gen):
+                    if iteration >= epoch_size:
+                        break
+                    images, targets = batch[0], batch[1]
+                    with torch.no_grad():
+                        if Cuda:
+                            images = Variable(torch.from_numpy(images).type(torch.FloatTensor)).cuda()
+                            targets = [Variable(torch.from_numpy(ann).type(torch.FloatTensor)).cuda() for ann in targets]
+                        else:
+                            images = Variable(torch.from_numpy(images).type(torch.FloatTensor))
+                            targets = [Variable(torch.from_numpy(ann).type(torch.FloatTensor)) for ann in targets]
+                    # 前向传播
+                    out = net(images)
+                    # 清零梯度
+                    optimizer.zero_grad()
+                    # 计算loss
+                    loss_l, loss_c = criterion(out, targets)
+                    loss = loss_l + loss_c
+                    # 反向传播
+                    loss.backward()
+                    optimizer.step()
+                    # 加上
+                    loc_loss += loss_l.item()
+                    conf_loss += loss_c.item()
 
-                print('\nEpoch:'+ str(epoch+1) + '/' + str(Epoch))
-                print('iter:' + str(iteration) + '/' + str(epoch_size) + ' || Loc_Loss: %.4f || Conf_Loss: %.4f ||' % (loc_loss/(iteration+1),conf_loss/(iteration+1)), end=' ')
+                    pbar.set_postfix(**{'loc_loss'  : loc_loss / (iteration + 1), 
+                                        'conf_loss' : conf_loss / (iteration + 1),
+                                        'lr'        : get_lr(optimizer)})
+                    pbar.update(1)
 
+            lr_scheduler.step()
             print('Saving state, iter:', str(epoch+1))
             torch.save(model.state_dict(), 'logs/Epoch%d-Loc%.4f-Conf%.4f.pth'%((epoch+1),loc_loss/(iteration+1),conf_loss/(iteration+1)))
