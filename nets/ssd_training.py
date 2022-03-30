44f8bb945143ca47f1ac0b3063005d67e0db99b4
Lee <lhy20001030@outlook.com>
2022-03-30 22:03:30 +0800

    Update ssd_training.py

--- nets/ssd_training.py
+++ nets/ssd_training.py
@@ -25,6 +25,7 @@ class MultiboxLoss(nn.Module):
     def _softmax_loss(self, y_true, y_pred):
         y_pred = torch.clamp(y_pred, min = 1e-7)
         print(y_true.shape)
+        print(y_true)
         print(y_pred.shape)
         softmax_loss = -torch.sum(y_true * torch.log(y_pred),
                                       axis=-1)
