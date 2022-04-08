4ebc5de6fcbef3d2673a07b0151f30db38892895
Hyfreadom <lhy20001030@outlook.com>
2022-04-08 10:26:48 +0800

    Update ssd.py

--- ssd.py
+++ ssd.py
@@ -32,7 +32,7 @@ class SSD(object):
         #   验证集损失较低不代表mAP较高，仅代表该权值在验证集上泛化性能较好。
         #   如果出现shape不匹配，同时要注意训练时的model_path和classes_path参数的修改
         #--------------------------------------------------------------------------#
-        "model_path"        : 'model_data/4-7.pth',
+        "model_path"        : 'model_data/ep32-4.8.pth',
         "classes_path"      : 'model_data/voc_classes.txt',
         #---------------------------------------------------------------------#
         #   用于预测的图像大小，和train时使用同一个即可
